9aab7d722c58b3a5b339fdc17546521d31d651a0
Jamie Scanlon <jamie@tenthlettermade.com>
2016-05-06 07:19:58 -0700

    Ad a stub for the main manager class.

---
+++ beauchamp/beauchamp/Beauchamp.swift
@@ -0,0 +1,13 @@
+//
+//  Beauchamp.swift
+//  beauchamp
+//
+//  Created by Jamie Scanlon on 5/4/16.
+//  Copyright © 2016 TenthLetterMade. All rights reserved.
+//
+
+import Foundation
+
+class Beauchamp {
+    
+}
